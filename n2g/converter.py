2930f0fbdcec30da2ec75fa8d43302ecb0f41171
Ben Mueller <code@madwonk.com>
2021-12-01 16:58:00 -0800

    Working rectangular regions!

--- n2g/converter.py
+++ n2g/converter.py
@@ -5,6 +5,7 @@ from geojson import FeatureCollection, Feature, Polygon
 from shapely.geometry.point import Point
 from shapely.ops import transform
 from functools import partial
+import numpy as np
 import pyproj
 import geojson
 import re
@@ -14,25 +15,31 @@ class n2g:
     def __init__(self, notams):
         self.notams = notams
 
+    def from_cord_to_dd(self, lat, lon):
+            sig = -1 if re.search('[swSW]', lat) else 1
+            if len(lat) <= 5:
+                lat = sig * (int(lat[:-3]) + float(lat[2:-1]) / 60)
+            else:
+                lat = sig * (int(lat[:-5]) + float(lat[2:-3]) / 60 + float(lat[4:-1]) / 360)
+
+            sig = -1 if re.search('[swSW]', lon) else 1
+            if len(lon) <= 6:
+                lon = sig * (int(lon[:-3]) + float(lon[2:-1]) / 60)
+            else:
+                lon = sig * (int(lon[:-5]) + float(lon[2:-3]) / 60 + float(lon[4:-1]) / 360)
+            return lat, lon
+
+
     def to_geojson(self):
         polygons = []
         for n in self.notams:
             decoded = notam.Notam.from_str(n)
 
-            poly = []
-            if decoded.poly is not None:
-                for point in decoded.poly:
-                    poly.append([point["lat"], point["long"]])
-
-
             area = decoded.area
             lat = area["lat"]
             lon = area["long"]
-            sig = -1 if re.search('[swSW]', lat) else 1
-            lat = sig * (int(lat[:-3]) + float(lat[2:-1]) / 60)
 
-            sig = -1 if re.search('[swSW]', lon) else 1
-            lon = sig * (int(lon[:-3]) + float(lon[2:-1]) / 60)
+            lat, lon = self.from_cord_to_dd(lat, lon)
 
             radius = area["radius"] # in nautical miles. WHAT THE FUCK Y U NO UZE METRIC
             radius *= 1852; # this is the really dumb simple way to get to meters
@@ -63,9 +70,18 @@ class n2g:
             polygons.append(Feature(geometry=Polygon([circle]),
                 properties={"title":str(decoded.notam_id), "description":str(decoded.purpose)}))
 
+
+            poly = []
             if decoded.poly is not None:
+                for point in decoded.poly:
+
+                    poly.append(self.from_cord_to_dd(point["long"], point["lat"])) # I don't know why these are backwards. But it seems to work
+
                 polygons.append(Feature(geometry=Polygon([poly]),
                     properties={"title":str(decoded.notam_id), "description":str(decoded.indices_item_e)}))
+
+                #np.append([polygons], [Feature(geometry=Polygon([poly]),
+                #    properties={"title":str(decoded.notam_id), "description":str(decoded.indices_item_e)})]).tolist()
 
 
             # decoded.area is only determined from q-clause
